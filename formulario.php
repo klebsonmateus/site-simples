bdd8e0343465e310ec3fc03936209f8ab5faab41
Klebson Mateus <klebsonbm@gmail.com>
2014-09-28 23:32:50 -0300

    Começando o formulário

---
+++ formulario.php
@@ -0,0 +1,25 @@
+<form class="form-horizontal">
+    <div class="control-group">
+        <label class="control-label" for="inputEmail">Email</label>
+        <div class="controls">
+            <input type="text" id="inputEmail" placeholder="Email">
+        </div>
+    </div>
+    <div class="control-group">
+        <label class="control-label" for="inputPassword">Senha</label>
+        <div class="controls">
+            <input type="password" id="inputPassword" placeholder="Senha">
+        </div>
+    </div>
+    <div class="control-group">
+        <label class="control-label" for="inputMessage">Mensagem</label>
+        <div class="controls">
+            <input type="text" id="inputMessage" placeholder="Mensagem" >
+        </div>
+    </div>
+    <div class="control-group">
+        <div class="controls">
+            <button type="submit" class="btn">Enviar</button>
+        </div>
+    </div>
+</form>
